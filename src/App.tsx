ec981dcbe879172fa125537c55f966e806f9fd4e
ahargunyllib <billy.bpm03@gmail.com>
2024-10-21 13:15:02 +0700

    feat: Add react-native-flash-message for displaying flash messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {View} from 'react-native';
 import ThemeProvider from './contexts/ThemeContext';
+import FlashMessage from 'react-native-flash-message';
 
 export default function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <ThemeProvider>
         <View>asd</View>
+        <FlashMessage
+          position="top"
+          floating={true}
+          animated={true}
+          duration={3000}
+        />
       </ThemeProvider>
     </NavigationContainer>
   );
