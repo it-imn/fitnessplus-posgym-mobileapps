d418eee94b9e4b3718d419972851d805fb1c8beb
user <billy.bpm03@gmail.com>
2024-10-21 12:36:32 +0700

    chore: add react navigation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View} from 'react-native';
+
+export default function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <View>asd</View>
+    </NavigationContainer>
+  );
+}
