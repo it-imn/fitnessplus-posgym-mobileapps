dc25b0ce3b8545a42a73e8b420bdbb44b7754656
ahargunyllib <billy.bpm03@gmail.com>
2024-10-22 11:31:27 +0700

    feat: Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,355 @@
+import LoadingGif from './gif/loading.json';
+import LogoSplash from './gif/logo.json';
+import LogoSplashWhite from './gif/logoWhite.json';
+import Verification from './gif/Verification.json';
+import Apple from './icons/Apple.svg';
+import Arrow from './icons/Arrow.svg';
+import ArrowBlack from './icons/ArrowBlack.svg';
+import ArrowRightWhite from './icons/ArrowRightWhite.svg';
+import ArrowWhite from './icons/ArrowWhite.svg';
+import Arrowyelow from './icons/Arrowyelow.svg';
+import AskIcon from './icons/AskIcon.svg';
+import BCA from './icons/BCA.png';
+import Biodata from './icons/Biodata.svg';
+import BMI from './icons/BMI.png';
+import BNI from './icons/BNI.png';
+import BRI from './icons/BRI.png';
+import Class from './icons/Class.png';
+import Classes from './icons/Classes.svg';
+import Consult from './icons/Consult.png';
+import Copy from './icons/Copy.svg';
+import CopyIcon from './icons/CopyIcon.svg';
+import CreditCard from './icons/CreditCard.png';
+import Danger from './icons/Danger.svg';
+import DeleteKey from './icons/DeleteKey.svg';
+import DummyCsConsult from './icons/DummyCsConsult.png';
+import DummyFoto from './icons/DummyFoto.jpg';
+import DummyPTConsult from './icons/DummyPTConsult.png';
+import EditPen from './icons/EditPen.svg';
+import EditPenWhite from './icons/EditPenWhite.svg';
+import Eggs from './icons/Eggs.svg';
+import Exercise from './icons/Exercise.svg';
+import Eye from './icons/Eye.svg';
+import Eyes from './icons/Eyes.svg';
+import EyeSee from './icons/EyeSee.svg';
+import EyeSeeWhite from './icons/EyeSeeWhite.svg';
+import EyeWhite from './icons/EyeWhite.svg';
+import FitnessPlusInd from './icons/FitnessPlusInd.png';
+import FitnessPlusIndWhite from './icons/FitnessPlusIndWhite.png';
+import FpiLogo from './icons/FpiLogo.svg';
+import Frequency from './icons/Frequency.svg';
+import Google from './icons/Google.svg';
+import Gopay from './icons/Gopay.png';
+import History from './icons/History.svg';
+import HistoryBlue from './icons/HistoryBlue.svg';
+import HistoryGrey from './icons/HistoryGrey.svg';
+import Historywh from './icons/Historywh.svg';
+import HomeBlue from './icons/HomeBlue.svg';
+import HomeGrey from './icons/HomeGrey.svg';
+import IconAcceptTransfer from './icons/IconAcceptTransfer.svg';
+import IconArrowDown from './icons/IconArrowDown.svg';
+import IconAnyTransfer from './icons/IconAnyTransfer.png';
+import IconCamera from './icons/IconCamera.svg';
+import IconCart from './icons/IconCart.svg';
+import IconCash from './icons/IconCash.svg';
+import IconChecklist from './icons/IconChecklist.svg';
+import IconChecklistBlack from './icons/IconChecklistBlack.svg';
+import IconClass from './icons/IconClass.svg';
+import IconClose from './icons/IconClose.svg';
+import IconCustomize from './icons/IconCostumize.svg';
+import IconDeleteAccount from './icons/IconDeleteAccount.svg';
+import IconDigitalPayment from './icons/IconDigitalPayment.svg';
+import IconDocument from './icons/IconDocument.svg';
+import IconDown from './icons/IconDown.svg';
+import IconInstructure from './icons/IconInstructure.svg';
+import IconInvited from './icons/IconInvited.png';
+import IconMember from './icons/IconMember.svg';
+import IconMembershipAgreement from './icons/IconMembershipAgreement.svg';
+import IconMenuTransfer from './icons/IconMenuTransfer.svg';
+import IconNotification from './icons/IconNotification.svg';
+import IconPaymentProses from './icons/IconPaymentProses.svg';
+import IconPlay from './icons/IconPlay.svg';
+import IconQrBlue from './icons/IconQrBlue.svg';
+import IconQrRed from './icons/IconQrRed.svg';
+import IconSave from './icons/IconSave.svg';
+import IconSend from './icons/IconSend.svg';
+import IconSendTransfer from './icons/IconSendTransfer.svg';
+import IconSetting from './icons/IconSetting.svg';
+import IconShare from './icons/IconShare.svg';
+import IconSilang from './icons/IconSilang.svg';
+import IconSuccess from './icons/IconSuccess.svg';
+import IconTheme from './icons/IconTheme.svg';
+import IconUp from './icons/IconUp.svg';
+import IconVerify from './icons/IconVerify.svg';
+import IconVoucher from './icons/IconVoucher.svg';
+import ImageClass from './icons/ImageClass.png';
+import ImageConfirmation from './icons/ImageConfirmation.png';
+import ImageNotFound from './icons/ImageNotFound.png';
+import ImagePT from './icons/ImagePT.png';
+import ImgInstruction from './icons/ImgInstruction.png';
+import Instagram from './icons/Instagram.svg';
+import InvitationIcon from './icons/InvitationIcon.svg';
+import LeftArrow from './icons/LeftArrow.svg';
+import Logo from './icons/Logo.svg';
+import Logo2 from './icons/Logo2.png';
+import LogoFpi from './icons/LogoFpi.png';
+import Logogmail from './icons/Logogmail.svg';
+import Logowa from './icons/Logowa.svg';
+import Mandiri from './icons/Mandiri.png';
+import Member from './icons/Member.svg';
+import Membership from './icons/Membership.png';
+import Merchandise from './icons/Merchandise.png';
+import Message from './icons/Message.svg';
+import NextArrow from './icons/NextArrow.svg';
+import Password from './icons/Password.svg';
+import Permata from './icons/Permata.png';
+import Person from './icons/Person.svg';
+import PersonalTrainerIcon from './icons/PersonalTrainerIcon.svg';
+import Power from './icons/Power.svg';
+import Profil from './icons/Profil.svg';
+import ProfileBlue from './icons/ProfileBlue.svg';
+import ProfileGrey from './icons/ProfileGrey.svg';
+import Progress from './icons/Progress.svg';
+import PT from './icons/PT.png';
+import Qrc from './icons/Qr.svg';
+import Report from './icons/Report.png';
+import ReportBlue from './icons/ReportBlue.svg';
+import ReportGrey from './icons/ReportGrey.svg';
+import RightArrow from './icons/RightArrow.svg';
+import RightArrowWhite from './icons/RightArrowWhite.svg';
+import Schedule from './icons/Schedule.png';
+import ShopeePay from './icons/ShopeePay.png';
+import StarIcon from './icons/StarIcon.svg';
+import TransferMembership from './icons/TransferMembership.png';
+import TransferPT from './icons/TransferPT.png';
+import User from './icons/User.png';
+import WhatsApp from './icons/WhatsApp.svg';
+import WhoGym from './icons/WhoGym.png';
+import IconQrWhite from './icons/IconQrWhite.svg';
+import Subscribe from './icons/subscribe.svg';
+import Approval from './icons/approval.svg';
+
+// image
+import SplashImage from './image/SplashScreen.png';
+import LogoP from './image/Logo.png';
+import ImageLogin from './image/imageLogin.png';
+import ImageSign from './image/SignScreen.png';
+import ImageLogo from './image/ImageLogo.png';
+import Onboarding1 from './image/onboarding1.png';
+import Onboarding2 from './image/onboarding2.png';
+import Onboarding3 from './image/onboarding3.png';
+import Attendance from './image/Attendance.png';
+import ClassImage from './image/Class.png';
+import MerchandiseImage from './image/Merchandise.png';
+import Personal from './image/PersonalTrainer.png';
+import Summary from './image/Summary.png';
+import WoG from './image/Whosongym.png';
+import SignAtom from './image/SignAtom.png';
+import MembershipNew from './image/MembershipNew.png';
+import WhoGymNew from './image/WhoGymNew.png';
+import ScheduleNew from './image/ScheduleNew.png';
+import ClassNew from './image/ClassNew.png';
+import BMINew from './image/BMINew.png';
+import PTNew from './image/PTNew.png';
+import MerchandiseNew from './image/MerchandiseNew.png';
+import ReportNew from './image/ReportNew.png';
+import ConsultNew from './image/ConsultNew.png';
+import BMINews from './image/BMINews.png';
+import ClassNews from './image/ClassNews.png';
+import ConsultNews from './image/ConsultNews.png';
+import MembershipNews from './image/MembershipNews.png';
+import MerchandiseNews from './image/MerchandiseNews.png';
+import PTNews from './image/PTNews.png';
+import ScheduleNews from './image/ScheduleNews.png';
+import SummaryNews from './image/SummaryNews.png';
+import WhoGymNews from './image/WhoGymNews.png';
+import HeaderHome from './image/HeaderHome.png';
+import BMITheme from './image/BMITheme.png';
+import ClassTheme from './image/ClassTheme.png';
+import ConsultTheme from './image/ConsultTheme.png';
+import MembershipTheme from './image/MembershipTheme.png';
+import MerchandiseTheme from './image/MerchandiseTheme.png';
+import PTTheme from './image/PTTheme.png';
+import ScheduleTheme from './image/ScheduleTheme.png';
+import SummaryTheme from './image/SummaryTheme.png';
+import WhoGymTheme from './image/WOGTheme.png';
+import IconCloseBlck from './icons/IconCloseBlck.svg';
+
+export {
+  Qrc,
+  History,
+  Historywh,
+  Profil,
+  Eyes,
+  Google,
+  Apple,
+  Eye,
+  User,
+  Member,
+  Message,
+  Classes,
+  Arrow,
+  Power,
+  EditPen,
+  Danger,
+  ArrowBlack,
+  Logogmail,
+  Logowa,
+  RightArrow,
+  LeftArrow,
+  DeleteKey,
+  CopyIcon,
+  FitnessPlusInd,
+  LogoFpi,
+  FpiLogo,
+  Person,
+  ArrowWhite,
+  Arrowyelow,
+  NextArrow,
+  Eggs,
+  Instagram,
+  WhatsApp,
+  ImgInstruction,
+  Logo,
+  Logo2,
+  HomeBlue,
+  ReportGrey,
+  ReportBlue,
+  ProfileGrey,
+  ProfileBlue,
+  HomeGrey,
+  StarIcon,
+  AskIcon,
+  InvitationIcon,
+  Copy,
+  RightArrowWhite,
+  Exercise,
+  PersonalTrainerIcon,
+  IconClass,
+  Progress,
+  Frequency,
+  IconSave,
+  IconShare,
+  IconPaymentProses,
+  IconSuccess,
+  IconCustomize,
+  EyeSee,
+  IconSend,
+  HistoryBlue,
+  HistoryGrey,
+  Biodata,
+  Password,
+  FitnessPlusIndWhite,
+  IconCamera,
+  IconInvited,
+  IconAnyTransfer,
+  IconDocument,
+  IconPlay,
+  LogoSplash,
+  LogoSplashWhite,
+  DummyFoto,
+  BMI,
+  Class,
+  Consult,
+  Membership,
+  Merchandise,
+  PT,
+  Report,
+  Schedule,
+  WhoGym,
+  ImageClass,
+  ImagePT,
+  IconClose,
+  IconQrBlue,
+  IconQrRed,
+  DummyCsConsult,
+  DummyPTConsult,
+  IconChecklist,
+  IconChecklistBlack,
+  ArrowRightWhite,
+  EditPenWhite,
+  EyeWhite,
+  EyeSeeWhite,
+  IconDeleteAccount,
+  ImageConfirmation,
+  ImageNotFound,
+  IconVoucher,
+  IconCash,
+  IconDigitalPayment,
+  LoadingGif,
+  BCA,
+  BRI,
+  BNI,
+  Mandiri,
+  Permata,
+  CreditCard,
+  Gopay,
+  ShopeePay,
+  IconArrowDown,
+  IconInstructure,
+  IconMember,
+  IconNotification,
+  IconCart,
+  IconMenuTransfer,
+  TransferMembership,
+  TransferPT,
+  IconAcceptTransfer,
+  IconSendTransfer,
+  IconTheme,
+  IconSetting,
+  Verification,
+  IconMembershipAgreement,
+  IconVerify,
+  IconUp,
+  IconDown,
+  IconSilang,
+  IconQrWhite,
+  Subscribe,
+  Approval,
+
+  // image
+  SplashImage,
+  LogoP,
+  ImageLogin,
+  ImageSign,
+  ImageLogo,
+  Onboarding1,
+  Onboarding2,
+  Onboarding3,
+  Attendance,
+  ClassImage,
+  MerchandiseImage,
+  Personal,
+  Summary,
+  WoG,
+  SignAtom,
+  MembershipNew,
+  WhoGymNew,
+  ScheduleNew,
+  ClassNew,
+  BMINew,
+  PTNew,
+  MerchandiseNew,
+  ReportNew,
+  ConsultNew,
+  BMINews,
+  ClassNews,
+  ConsultNews,
+  MembershipNews,
+  MerchandiseNews,
+  PTNews,
+  ScheduleNews,
+  SummaryNews,
+  WhoGymNews,
+  HeaderHome,
+  BMITheme,
+  ClassTheme,
+  ConsultTheme,
+  MembershipTheme,
+  MerchandiseTheme,
+  PTTheme,
+  ScheduleTheme,
+  SummaryTheme,
+  WhoGymTheme,
+  IconCloseBlck,
+};
