95af8d3cebc63e3bb26cd710e82a9b39d9743e2b
ahargunyllib <billy.bpm03@gmail.com>
2025-02-27 15:54:36 +0700

    fix: lower password char validation

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -31,7 +31,7 @@ import { setNotificationsHandler } from "../../lib/notification";
 
 const loginSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
+  password: z.string().min(4, "Password must be at least 4 characters"),
 });
 
 export const LoginPage = ({
